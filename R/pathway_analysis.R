6bec4ece6e9300a75222dc6ad5433eee63df1817
Anil Chalisey <anilchalisey@gmail.com>
2018-09-25 10:44:39 +0100

    Fixed bug in pathway_analysis()

--- R/pathway_analysis.R
+++ R/pathway_analysis.R
@@ -13,6 +13,8 @@
 #' @import org.Mm.eg.db
 #' @importFrom dplyr mutate filter select
 #' @importFrom utils write.table
+#' @importFrom AnnotationDbi mapIds
+#' @import pathview
 
 
 pathway_analysis <- function(x, species = c("human", "mouse")) {
@@ -20,40 +22,92 @@ pathway_analysis <- function(x, species = c("human", "mouse")) {
   . <- q.val <- kegg.id <- pathway <- GO.id <- NULL
   
   match.arg(species, c("human", "mouse"))
-  if (species == "human") {kid <- "hsa"}
-  if (species == "mouse") {kid <- "mmu"}
-
+  if (species == "human") {
+    kid <- "hsa"
+    org <- org.Hs.eg.db::org.Hs.eg.db
+  }
+  if (species == "mouse") {
+    kid <- "mmu"
+    org <- org.Mm.eg.db::org.Mm.eg.db
+  }
+  
+  # KEGG/GO rely on ENTREZ ID, so we convert the ENSEMBL ID
+  x$symbol <- AnnotationDbi::mapIds(
+    org,
+    keys = row.names(x),
+    column = "SYMBOL",
+    keytype = "ENSEMBL",
+    multiVals = "first"
+  )
+  x$entrez <- AnnotationDbi::mapIds(
+    org,
+    keys = row.names(x),
+    column = "ENTREZID",
+    keytype = "ENSEMBL",
+    multiVals = "first"
+  )
+  x$name <- AnnotationDbi::mapIds(
+    org,
+    keys = row.names(x),
+    column = "GENENAME",
+    keytype = "ENSEMBL",
+    multiVals = "first"
+  )
+  
+  # convert the name of the log2FoldChange column to logFC
+  colnames(x) <- gsub("log2FoldChange", "logFC", colnames(res))
+  
+  # Obtain a vector fold-changes where the name of the values are ENTREZ IDs
+  foldchanges <- x$logFC
+  names(foldchanges) <- x$entrez
+  
+  # KEGG pathway analysis first ---------------
+  # Generate uptodata KEGG pathway gene sets
   kg <- gage::kegg.gsets(species = kid)
+  
+  # Subset for signalling and metabolic pathways only
   kegg.sigmet.gs <- kg$kg.sets[kg$sigmet.idx]
-  kegg.dise.gs <- kg$kg.sets[kg$dise.idx]
-
+  
+  # Get the results - here we use same.dir = TRUE to get separate lists for 
+  # pathways that are upregulated and downregulated
+  keggres <- gage::gage(foldchanges, gsets = kegg.sigmet.gs, same.dir = TRUE)
+  
+  # if plotting then do so here - we plot the top 3 upregulated and downregulated
+  if (plot {
+    keggrespathways.up <- data.frame(id = rownames(keggres$greater), keggres$greater) %>%
+      tbl_df() %>% 
+      filter(row_number() <= 3) %>% 
+      .$id %>% 
+      as.character()
+    keggrespathways.down <- data.frame(id = rownames(keggres$less), keggres$less) %>%
+      tbl_df() %>% 
+      filter(row_number() <= 3) %>% 
+      .$id %>% 
+      as.character()
+    keggrespathways <- c(keggrespathways.up, keggrespathways.down)
+    keggresids <- substr(keggrespathways, start = 1, stop = 8)
+    ## Unfortunately, the code for pathview (specifically the geneannot.map() function)
+    ## down't specify AnnotationDbi::select at line 52, and so this conflicts with the 
+    ## dplyr namespace, which we need to unload
+    if(any(grepl("package:dplyr", search()))) {
+      detach("package:dplyr")
+      dplyr.unloaded <- TRUE
+    }
+    tmp <- sapply(keggresids, function(pid) {
+      pathview::pathview(gene.data = foldchanges, pathway.id = pid, species = kid)
+    })
+    ## Re-attach dplyr if it was unloaded
+    if (dplyr.unloaded) require("dplyr", quietly = TRUE)
+  }
+  
+  # Gene Ontology analysis; we only do the BP ---------------
   go <- gage::go.gsets(species = species)
   go.bp.gs <- go$go.sets[go$go.subs$BP]
-  go.mf.gs <- go$go.sets[go$go.subs$MF]
-  go.cc.gs <- go$go.sets[go$go.subs$CC]
-
-  res <- as.data.frame(x)
-  rownames(res) <- res$gene
-
-  colnames(res) <- gsub("log2FoldChange", "logFC", colnames(res))
-  colnames(res) <- gsub("FDR", "padj", colnames(res))
   
-  res <- ens2entrez(
-    result = res,
-    species = "human",
-    columns.of.interest = c("gene", "logFC", "FDR"),
-    colnames = c("gene", "logFC", "padj", "entrez")
-  )
-
-  fc.res <- res$logFC
-  names(fc.res) <- res$entrez
-
-  fc.kegg.sigmet.p <- gage::gage(fc.res, gsets = kegg.sigmet.gs)
-  fc.kegg.dise.p <- gage::gage(fc.res, gsets = kegg.dise.gs)
-  fc.go.bp.p <- gage::gage(fc.res, gsets = go.bp.gs)
-  fc.go.mf.p <- gage::gage(fc.res, gsets = go.mf.gs)
-  fc.go.cc.p <- gage::gage(fc.res, gsets = go.cc.gs)
-
+  gobres <- gage::gage(foldchanges, gsets = go.bp.gs, same.dir = TRUE)
+  
+  
+  # Write out the results -----------------------------------
   kegg.as.dataframe <- function(x, direction) {
     as.data.frame(x[[direction]]) %>%
     dplyr::mutate(kegg.id = unlist(lapply(strsplit(rownames(.), " "), function(x) x[[1]])),
@@ -62,11 +116,9 @@ pathway_analysis <- function(x, species = c("human", "mouse")) {
     dplyr::filter(!is.na(q.val)) %>% dplyr::select(kegg.id, pathway, dplyr::everything())
   }
 
-  fc.kegg.sigmet.p.up <- kegg.as.dataframe(fc.kegg.sigmet.p, "greater")
-  fc.kegg.sigmet.p.down <- kegg.as.dataframe(fc.kegg.sigmet.p, "less")
-  fc.kegg.dise.p.up <- kegg.as.dataframe(fc.kegg.dise.p, "greater")
-  fc.kegg.dise.p.up <- kegg.as.dataframe(fc.kegg.dise.p, "less")
-
+  kegg.up <- kegg.as.dataframe(keggres, "greater")
+  kegg.down <- kegg.as.dataframe(keggres, "less")
+  
   GO.as.dataframe <- function(x, direction) {
     as.data.frame(x[[direction]]) %>%
       dplyr::mutate(GO.id = unlist(lapply(strsplit(rownames(.), " "), function(x) x[[1]])),
@@ -75,15 +127,11 @@ pathway_analysis <- function(x, species = c("human", "mouse")) {
       dplyr::filter(!is.na(q.val)) %>% dplyr::select(GO.id, pathway, dplyr::everything())
   }
 
-  fc.go.bp.p.up <- GO.as.dataframe(fc.go.bp.p, "greater")
-  fc.go.mf.p.up <- GO.as.dataframe(fc.go.mf.p, "greater")
-  fc.go.cc.p.up <- GO.as.dataframe(fc.go.cc.p, "greater")
-  fc.go.bp.p.down <- GO.as.dataframe(fc.go.bp.p, "less")
-  fc.go.mf.p.down <- GO.as.dataframe(fc.go.mf.p, "less")
-  fc.go.cc.p.down <- GO.as.dataframe(fc.go.cc.p, "less")
-
-  towrite = ls(pattern = "fc.go.*up|fc.go.*down|fc.kegg.*up|fc.kegg.*down")
-  names(towrite) <- towrite
+  GO.up <- GO.as.dataframe(gobres, "greater")
+  GO.down <- GO.as.dataframe(gobres, "less")
+  
+  towrite <- list(kegg.up, kegg.down, GO.up, GO.down)
+  names(towrite) <- c("kegg.up", "kegg.down", "GO.up", "GO.down")
   create_dir("pathway_results")
 
   sapply(names(towrite), function(x) {
@@ -92,5 +140,10 @@ pathway_analysis <- function(x, species = c("human", "mouse")) {
                 row.names = FALSE, col.names = TRUE, sep = "\t",
                 quote = FALSE)
   })
+  
+  tomove1 <- list.files(".", pattern = paste0(kid, ".*\\.png"))
+  tomove2 <- list.files(".", pattern = paste0(kid, ".*\\.xml"))
+  file.rename(from = tomove1, to = file.path("pathway_results", tomove1))
+  file.rename(from = tomove2, to = file.path("pathway_results", tomove2))
   return(towrite)
 }
